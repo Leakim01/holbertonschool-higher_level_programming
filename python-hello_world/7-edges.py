066a6e393684a5fb5654ccc242d4400242eeb0b8
Leakim01 <5662@holbertonstudents.com>
2023-01-16 06:34:44 -0800

    Change Numnbers

--- python-hello_world/7-edges.py
+++ python-hello_world/7-edges.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 word = "Holberton"
 print(f"First 3 letters: {word[:3]}")
-print(f"Last 2 letters: {word[-2:9]}")
+print(f"Last 2 letters: {word[-2:-1]}")
 print(f"Middle word: {word[-8:8]}")
 
 
