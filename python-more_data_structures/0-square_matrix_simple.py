c25a054df812bb5a3975f0df4b536b1f035071f1
Leakim01 <5662@holbertonstudents.com>
2023-01-25 07:29:53 -0800

    Task 0 - Create a New Matrix

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
+    new_list = []
+    for each_list in matrix:
+        new_list = []
+        for each_elem in each_list:
+            square = each_elem ** 2
+            new_list.append(square)
+        new_matrix.append(new_list)
+    return new_matrix
