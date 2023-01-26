734a699624257ec62420f96ba194985ccb06b208
Leakim01 <5662@holbertonstudents.com>
2023-01-26 02:18:20 -0800

    Task 2 - Add Numbers Except Similar Values

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,6 +1,6 @@
 def search_replace(my_list, search, replace):
     new_list = my_list.copy()
-    for i in range(len(new_list)):
-        if new_list[i] == search:
+    for i, value in enumerate(new_list):
+        if value == search:
             new_list[i] = replace
     return new_list
