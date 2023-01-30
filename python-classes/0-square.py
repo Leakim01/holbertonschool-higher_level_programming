b3474122caada420dcad8761c475b918b0671553
Leakim01 <5662@holbertonstudents.com>
2023-01-30 04:51:11 -0800

    Task 0 - Empty Class

---
+++ python-classes/0-square.py
@@ -0,0 +1,4 @@
+class Square:
+
+    """Square is empty"""
+    """I don't know why we do this for nothing"""
--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def replace_in_list(my_list, idx, element):
+def print_reversed_list_integer(my_list=[]):
     if idx < 0:
         return my_list
     if idx > len(my_list) - 1:
