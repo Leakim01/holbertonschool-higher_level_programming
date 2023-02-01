a2b1d5d46ce9a137052aa684bd3eba2b2e28e680
Leakim01 <5662@holbertonstudents.com>
2023-02-01 06:16:03 -0800

    Task 1 - Square Size

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 """Bin Bash for .py Files"""
+
+
 class Square:
     """
     Attributes:
         size (str): The size of the square"""
+        
     def __init__(self, size):
         """Args:
         size (str): The size of the square"""
