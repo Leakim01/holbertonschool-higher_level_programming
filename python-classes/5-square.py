8fa59e60ddde478220f0a74477b25dc663bc7d1a
Leakim01 <5662@holbertonstudents.com>
2023-02-02 06:46:01 -0800

    Task 5 - Square

---
+++ python-classes/5-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Bin Bash for .py Files"""
+
+
+class Square:
+    """
+    Attributes:
+        size (str): The size of the square"""
+
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        for h in range(self.__size):
+            for v in range(h):
+                print("#", end="")
+            print("")
+            if self.__size == 0:
+                print("")
