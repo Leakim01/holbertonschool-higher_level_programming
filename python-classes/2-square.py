2465457446048b83d1198f6895a04ce476fc99d6
Leakim01 <5662@holbertonstudents.com>
2023-02-02 05:19:34 -0800

    Task 2 - If Raise

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 """Bin Bash for .py Files"""
 
+
 class Square:
     """
     Attributes:
         size (str): The size of the square"""
 
-
     def __init__(self, size=0):
         if type(size) is not int:
             raise TypeError("size must be an integer")
         if size < 0:
-            raise ValueError ("size must be >= 0")
+            raise ValueError("size must be >= 0")
         self.__size = size
