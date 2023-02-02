856bbc374b2affad6b6dd233e9436f53170f2f29
Leakim01 <5662@holbertonstudents.com>
2023-02-02 05:28:35 -0800

    Task 3 - Return Area

---
+++ python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Bin Bash for .py Files"""
+
+
+class Square:
+    """
+    Attributes:
+        size (str): The size of the square"""
+
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return self.size **2
