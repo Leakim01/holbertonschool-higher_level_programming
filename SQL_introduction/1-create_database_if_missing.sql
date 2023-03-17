df73ddcbb8736a32d4f5f4b0f2df15dfcccf9879
Leakim01 <5662@holbertonstudents.com>
2023-03-17 03:31:45 -0700

    Task 1 - Create Database If Missing

--- SQL_introduction/1-create_database_if_missing.sql
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -1,4 +1,3 @@
 -- Create a Database if it doesn't exist
 -- If it Already exists, Script will should not fail !
--- Askip faut rajouter une PUTAIN DE TROISIEME LIGNE !!
-CREATE DATABASE IF NOT EXIST hbtn_0c_0;
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
