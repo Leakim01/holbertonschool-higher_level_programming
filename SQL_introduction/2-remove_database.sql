abbd200e687e07369f9400ca1c8e804d3cde6721
Leakim01 <5662@holbertonstudents.com>
2023-03-17 03:37:48 -0700

    Task 2 - Remove a Database

--- SQL_introduction/2-remove_database.sql
+++ SQL_introduction/2-remove_database.sql
@@ -1,6 +1,5 @@
 -- First, Creation of a Database.
 -- Second, Create it if not already exists.
 -- And finally, Removing it.
-CREATE DATABASE hbtn_0c_0;
 CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
 DROP DATABASE hbtn_0c_0;
